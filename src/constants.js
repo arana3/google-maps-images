1f3d9c059030dd3d1c0744282efd09173295984e
Michael Hogg <michael.hogg.uk@gmail.com>
2016-12-17 15:57:26 +0000

    Create src/constants.js

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// @flow
+
+/** @see developers.google.com/maps/documentation/static-maps/intro#URL_Parameters */
+export const BASE_URL_STATICMAP: string = 'https://maps.googleapis.com/maps/api/staticmap?';
