5ce9f8e9a8b11d290506e050d8b25169331424e3
Michael Hogg <michael.hogg.uk@gmail.com>
2016-12-17 17:06:59 +0000

    Create src/index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// @flow
+
+import StaticMap,                 { makeStaticMap                 } from './static-map/static-map.js';
+import StaticMapLocation,         { makeStaticMapLocation         } from './static-map/location.js';
+import StaticMapMarkerCollection, { makeStaticMapMarkerCollection } from './static-map/marker-collection.js';
+import StaticMapStyle,            { makeStaticMapStyle            } from './static-map/style.js';
+
+export {
+
+    StaticMap,
+    StaticMapLocation,
+    StaticMapMarkerCollection,
+    StaticMapStyle,
+
+    makeStaticMap,
+    makeStaticMapLocation,
+    makeStaticMapMarkerCollection,
+    makeStaticMapStyle
+
+};
