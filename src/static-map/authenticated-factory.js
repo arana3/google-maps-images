3ba6344e2bf40daf056c79c158eaf5005b4a48ff
Michael Hogg <michael.hogg.uk@gmail.com>
2016-12-26 17:01:16 +0000

    src/static-map/authenticated-factory.js: add "client credentials" auth

--- src/static-map/authenticated-factory.js
+++ src/static-map/authenticated-factory.js
@@ -1,22 +1,55 @@
 // @flow
 
-import StaticMap                                                    from './static-map.js';
-import StaticMapLocation,         { makeStaticMapLocation         } from './location.js';
-import StaticMapMarkerCollection, { makeStaticMapMarkerCollection } from './marker-collection.js';
-import StaticMapStyle,            { makeStaticMapStyle            } from './style.js';
+//----------------------------------------------------------------------//
+//                         Import local modules                         //
+//----------------------------------------------------------------------//
+
+import StaticMap                                                     from './static-map.js';
+import StaticMapLocation,         { makeStaticMapLocation          } from './location.js';
+import StaticMapMarkerCollection, { makeStaticMapMarkerCollection  } from './marker-collection.js';
+import StaticMapStyle,            { makeStaticMapStyle             } from './style.js';
+import type { TypeAuthDataApiKey, TypeAuthDataClient, TypeAuthData } from './types.js';
+
+
+//----------------------------------------------------------------------//
+//                            Exported class                            //
+//----------------------------------------------------------------------//
 
 export default class StaticMapAuthenticatedFactory {
 
-    apiKey: string;
+    auth: TypeAuthData;
 
-    constructor(apiKey: string) {
-        this.apiKey = apiKey;
+    /** @private */
+    constructor(auth: TypeAuthData) {
+        this.auth = auth;
+    }
+
+    static usingApiKey(apiKey: string): StaticMapAuthenticatedFactory {
+        const auth: TypeAuthDataApiKey = { type: 'apiKey', apiKey };
+        return new StaticMapAuthenticatedFactory(auth);
+    }
+
+    static usingClientCredentials(clientId: string, clientSecret: string): StaticMapAuthenticatedFactory {
+        const auth: TypeAuthDataClient = { type: 'client', clientId, clientSecret };
+        return new StaticMapAuthenticatedFactory(auth);
     }
 
     makeStaticMap(width: number, height: number): StaticMap {
+
         const staticMap = new StaticMap(width, height);
-        staticMap.apiKey(this.apiKey);
+
+        /** @see flowtype.org/docs/disjoint-unions.html */
+        switch (this.auth.type) {
+            case 'apiKey':
+                staticMap.setApiKey(this.auth.apiKey);
+                break;
+            case 'client':
+                staticMap.setClientCredentials(this.auth.clientId, this.auth.clientSecret);
+                break;
+        }
+
         return staticMap;
+
     }
 
     makeStaticMapLocation         = makeStaticMapLocation;
